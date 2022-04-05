612f3a26278279812eee603e3c6c3b01c6315c67
kangminseok <mskang85@bestfin.co.kr>
2022-04-05 16:19:11 +0900

    dev

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,3 +8,13 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
